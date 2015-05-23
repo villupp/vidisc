09c642cdfbc16715ee90ff903a6494ade37a0943
Ville Piirainen <v.piirainen@netti.fi>
2015-05-23 11:39:09 +0300

    fix

--- public/js/scorecard.js
+++ public/js/scorecard.js
@@ -1,6 +1,7 @@
 var currentRoundScores = getCookie('currentRoundScores');
 
 function scorecard() {
-	console.log(JSON.parse(currentRoundScores));
+	console.log(JSON.stringify(currentRoundScores));
+	console.log(currentRoundScores.playedAt);
 	$('course-container').append(currentRoundScores);
 }
