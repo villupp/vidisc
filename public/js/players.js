15fd228a5af7b8a08d79de6561ffbc3711464890
Ville Piirainen <v.piirainen@netti.fi>
2015-05-24 13:11:30 +0300

    Fixed player adding, still very buggy.

--- public/js/players.js
+++ public/js/players.js
@@ -1,8 +1,44 @@
 function addPlayer() {
 	var playerName = $('#new-player-name').val();
-	if ( confirm('Are you sure you want to add new player "' + playerName + '"') ) {
-		var requestURL = 'http://discgolfapi-vpii.rhcloud.com/discgolfapi/disc/api/addplayer?name=' + playerName;
-		console.log(requestURL);
+	//if ( confirm('Are you sure you want to add new player "' + playerName + '"') ) {
+	
+	var requestURL = 'http://discgolfapi-vpii.rhcloud.com/discgolfapi/disc/api/addplayer';
+	var data = { name : playerName };
+	console.log(requestURL);
+	$.ajax({
+		type: 'POST',
+		url: requestURL,
+	    data: data,
+		success: function(response) {
+			//var roundJSON = JSON.parse(roundObject);
+			console.log(response.responseText);
+		},
+		error: function(error) {
+			console.log("Error occured when adding player: " + error.responseText);
+		},
+	});
+
+
+		/*
+		$.get(requestURL, function(data) {
+  			console.log("DATA : " + data);
+		});
+
+		var jqxhr = $.get(requestURL, function() {
+	    	alert("success" + JSON.stringify(data));
+	  	})
+	    .done(function(data) {
+	      	alert("second success" + JSON.stringify(data));
+	    })
+	    .fail(function(data) {
+	      	alert("error" + JSON.stringify(data));
+	    })
+	    .always(function(data) {
+	      	alert("finished" + JSON.stringify(data));
+	    });
+
+	    */
+		/*console.log(requestURL);
 	    var xhr = createCORSRequest('GET',  requestURL);
 	    if (!xhr) {
 	        throw new Error('CORS not supported');
@@ -17,7 +53,7 @@ function addPlayer() {
 	    	console.log(error);
 	        console.log('There was an error!');
 	    };
-	    xhr.send();
+	    xhr.send();*/
 		/*
 	    $.ajax({
   			type: 'GET',
@@ -34,5 +70,5 @@ function addPlayer() {
 			}
 		});
 		*/
-	}
+	//}
 }
